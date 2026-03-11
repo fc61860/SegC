aa53bf0dd004a31fb0bf481db983e26d5f27a9f6
FC61813 <fc61813@alunos.fc.ul.pt>
2026-03-11 11:30:50 +0000

    Adiciona loop para leitura de comandos do usuário e exibe menu de comandos após login

--- SpertaClient/src/client/myClient.java
+++ SpertaClient/src/client/myClient.java
@@ -57,44 +57,58 @@ public class myClient {
                 
         if(sucesso) {
             System.out.println("Login efetuado com sucesso!");
+            Boolean running = true;
+            
+            mostrarMenu();
 
-            File fileSend = new File("SpertaServer/data/teste.txt");
-            if(fileSend.exists()) {
-                outStream.writeObject(fileSend.getName());
-                outStream.writeObject(fileSend.length());
+            // ler os comandos do user
+            while(running) {
+                String input = sc.nextLine().trim();
 
-                FileInputStream fileIn = new FileInputStream(fileSend);
-                byte[] buffer = new byte[1024];
-                int bytesRead;
+                if(input.isEmpty()) continue;
 
-                while ((bytesRead = fileIn.read(buffer)) != -1) {
-                        outStream.write(buffer, 0, bytesRead);
-                    }
-
-                outStream.flush(); // Garante que o último bocado de bytes é empurrado pelo tubo
-                fileIn.close();
-                System.out.println("Ficheiro enviado com sucesso!");
-            } else {
-                System.out.println("O ficheiro não existe!");
+                String[] parts = input.split(" ");
+                String order = parts[0].toUpperCase();
+                
             }
 
-            try {
-                String fileName = (String) inStream.readObject();
-                long fileSize = (Long) inStream.readObject();
-                FileOutputStream fileOut = new FileOutputStream("returned_" + fileName);
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                long totalLido = 0;
+            // File fileSend = new File("SpertaServer/data/teste.txt");
+            // if(fileSend.exists()) {
+            //     outStream.writeObject(fileSend.getName());
+            //     outStream.writeObject(fileSend.length());
 
-                while (totalLido < fileSize && (bytesRead = inStream.read(buffer, 0, (int)Math.min(buffer.length, fileSize - totalLido))) != -1) {
-                    fileOut.write(buffer, 0, bytesRead);
-                    totalLido += bytesRead;
-                }
+            //     FileInputStream fileIn = new FileInputStream(fileSend);
+            //     byte[] buffer = new byte[1024];
+            //     int bytesRead;
 
-                fileOut.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            //     while ((bytesRead = fileIn.read(buffer)) != -1) {
+            //             outStream.write(buffer, 0, bytesRead);
+            //         }
+
+            //     outStream.flush(); // Garante que o último bocado de bytes é empurrado pelo tubo
+            //     fileIn.close();
+            //     System.out.println("Ficheiro enviado com sucesso!");
+            // } else {
+            //     System.out.println("O ficheiro não existe!");
+            // }
+
+            // try {
+            //     String fileName = (String) inStream.readObject();
+            //     long fileSize = (Long) inStream.readObject();
+            //     FileOutputStream fileOut = new FileOutputStream("returned_" + fileName);
+            //     byte[] buffer = new byte[1024];
+            //     int bytesRead;
+            //     long totalLido = 0;
+
+            //     while (totalLido < fileSize && (bytesRead = inStream.read(buffer, 0, (int)Math.min(buffer.length, fileSize - totalLido))) != -1) {
+            //         fileOut.write(buffer, 0, bytesRead);
+            //         totalLido += bytesRead;
+            //     }
+
+            //     fileOut.close();
+            // } catch (Exception e) {
+            //     e.printStackTrace();
+            // }
 
             
 
@@ -115,5 +129,20 @@ public class myClient {
         }
 
 
+    }
+
+    private void mostrarMenu() {
+        System.out.println("\n=========================================================================");
+        System.out.println("                            MENU DE COMANDOS                             ");
+        System.out.println("=========================================================================");
+        System.out.println(" CREATE <hm>         | Criar casa <hm> (Ficas como Owner)");
+        System.out.println(" ADD <user> <hm> <s> | Adicionar <user> à casa <hm>, secção <s>");
+        System.out.println(" RD <hm> <s>         | Registar um Dispositivo na casa <hm>, secção <s>");
+        System.out.println(" EC <hm> <d> <int>   | Enviar valor <int> ao dispositivo <d> da casa <hm>");
+        System.out.println(" RT <hm>             | Receber último estado dos dispositivos da casa <hm>");
+        System.out.println(" RH <hm> <d>         | Receber Histórico (.csv) do dispositivo <d>");
+        System.out.println(" EXIT                | Sair da aplicação");
+        System.out.println("=========================================================================");
+        System.out.print("Introduza um comando: ");
     }
 }
