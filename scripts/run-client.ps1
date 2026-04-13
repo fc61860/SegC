123e5db8eb4c2e5afe8f70617644c6e0b1b5e503
FC61813 <fc61813@alunos.fc.ul.pt>
2026-04-13 23:12:27 +0100

    atualização dos scripts

--- scripts/run-client.ps1
+++ scripts/run-client.ps1
@@ -3,6 +3,18 @@ param(
     [string]$ServerAddress,
 
     [Parameter(Mandatory = $true)]
+    [string]$Truststore,
+
+    [Parameter(Mandatory = $true)]
+    [string]$PasswordTruststore,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Keystore,
+
+    [Parameter(Mandatory = $true)]
+    [string]$PasswordKeystore,
+
+    [Parameter(Mandatory = $true)]
     [string]$UserName,
 
     [Parameter(Mandatory = $true)]
@@ -11,6 +23,6 @@ param(
 
 $ErrorActionPreference = 'Stop'
 
-& java -jar 'SpertaClient/bin/SpertaClient.jar' $ServerAddress $UserName $Password
+& java -cp 'SpertaClient/bin/classes' 'SpertaClient.src.client.SpertaClient' $ServerAddress $Truststore $PasswordTruststore $Keystore $PasswordKeystore $UserName $Password
 
 exit $LASTEXITCODE
--- scripts/run-server.ps1
+++ scripts/run-server.ps1
@@ -1,9 +1,19 @@
 param(
-    [int]$Port = 22345
+    [int]$Port = 22345,
+
+    [Parameter(Mandatory = $true)]
+    [string]$PasswordCifra,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Keystore,
+
+    [Parameter(Mandatory = $true)]
+    [string]$PasswordKeystore
 )
 
 $ErrorActionPreference = 'Stop'
 
-& java -cp 'SpertaServer/bin' 'SpertaServer' $Port
+# Usamos as aspas para garantir que os argumentos são passados corretamente
+& java -cp 'SpertaServer/bin' 'SpertaServer' $Port $PasswordCifra $Keystore $PasswordKeystore
 
 exit $LASTEXITCODE
