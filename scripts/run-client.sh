78efbdf93c966adf559ed922ef1c178d3f6dcdb5
João Rodrigues <joaorodrigues1000000@gmail.com>
2026-04-15 16:48:57 +0100

    .

--- scripts/run-client.sh
+++ scripts/run-client.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 if [ "$#" -ne 7 ]; then
     echo "Uso: ./scripts/run-client.sh <serverAddress> <truststore> <password-truststore> <keystore> <password-keystore> <user-id> <password>" >&2
+    echo "./scripts/run-client.sh localhost ./security/client-truststore.jks changeit ./security/client1-keystore.p12 changeit client1 changeit" >&2
     exit 1
 fi
 
